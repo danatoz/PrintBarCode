2b636d5e3139dcb48af311e5f91f7df5410ea2b3
Edward <gulyaev.ed.v@gmail.com>
2021-02-09 12:45:11 +0400

    Тесты, незначительные изменения

---
+++ Test/DateCodes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    public class DateCodes : IEquatable<DateCodes>
+    {
+        public string Year { get; set; }
+        public string Month { get; set; }
+
+
+        public bool Equals(DateCodes other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dateDay = DateTime.Now.ToString("dd");
+            string dateMonth = DateTime.Now.ToString("MM");
+            string dateYear = DateTime.Now.ToString("yyyy");
+            string Date = $"{dateDay} {dateMonth} {dateYear}";
+            List<List<string>> dateList = new List<List<string>>()
+            {
+                new List<string>{"2021"},
+                new List<string>{"137", "138","139","140","173","174","175","176","177","178","179","180"}
+            };
+
+            Console.Write(dateList[0]);
+            
+
+
+            //Console.Write(Date);
+
+            Console.ReadKey();
+        }
+    }
+}
